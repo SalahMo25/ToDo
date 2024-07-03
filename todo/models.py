84518da054d325979c1ca9cea78574b93f3c39f3
SalahMo25 <salahmohmed504@gmail.com>
2024-07-03 04:01:00 +0300

    v1

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Todo(models.Model):
+    text = models.CharField(max_length=300)
+    date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.date
+    
+    
